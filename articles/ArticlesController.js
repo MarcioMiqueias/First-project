51bbe9fdea571b33abe815fece454834c6da8e07
Marcio Miqueias G Inacio <mmgi.marcio@gmail.com>
2022-10-25 11:28:08 -0300

    Articles edit
    Articles edit

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -53,5 +53,40 @@ router.post("/articles/delete", (req, res) => {
     }
 });
 
+router.get("/admin/articles/edit/:id", (req, res) => {
+    var id = req.params.id;
+    if(isNaN(id)){
+        res.redirect("/admin/articles");
+    }
+    Article.findByPk(id).then(article => {
+        if(article != undefined){
+            Category.findAll().then(categories => {
+                res.render("admin/articles/edit", {article: article, categories: categories})
+            });
+        }else{
+            res.redirect("/admin/articles");
+        }
+    }).catch(error => {
+        res.redirect("/admin/articles");
+    });
+});
+
+router.post("/articles/update", (req, res) => {
+    var id = req.body.id;
+    var title = req.body.title;
+    var body = req.body.body;
+    var category = req.body.category
+
+    Article.update({title: title, slug: slugify(title), body: body, categoryId: category},{
+        where: {
+            id: id
+        }
+    }).then(() => {
+        res.redirect("/admin/articles");
+    }).catch(error => {
+        res.redirect("/");
+    });
+});
+
 
 module.exports = router;
